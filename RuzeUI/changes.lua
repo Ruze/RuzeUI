f9c88caf37e412c7c831e9f98ad16118fe35a12b
Ruze <ruze.360@gmai.com>
2012-09-15 19:14:08 +1000

    Clean up files

--- RuzeUI/changes.lua
+++ RuzeUI/changes.lua
@@ -70,27 +70,10 @@ for _, frame in pairs(units) do
 	local self = _G[t..frame]
 	local unit = string.lower(frame)
 
-	--if unit == "player" then		
-		--self:ClearAllPoints()
-		--self:SetPoint("BOTTOMLEFT", InvTukuiActionBarBackground, "TOPLEFT", 70, 8)
-		
-		--[[local buffs = CreateFrame("Frame", nil, self)
-		buffs:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", 0, 4)
-		buffs:SetHeight(26)
-		buffs:SetWidth(252)
-		buffs.size = 26
-		buffs.num = 36
-		buffs.numRow = 9
-		buffs.spacing = 2
-		buffs.initialAnchor = "TOPRIGHT"
-		buffs["growth-y"] = "UP"
-		buffs["growth-x"] = "LEFT"
-		buffs.PostCreateIcon = T.PostCreateAura
-		--buffs.PostUpdateIcon = T.PostUpdateAura
-		--buffs.PostUpdate = T.UpdateTargetDebuffsHeader
-		G.UnitFrames.Player.Buffs = buffs]]
-	--end
-
+	if unit == "player" then		
+		self:ClearAllPoints()
+		self:SetPoint("BOTTOMLEFT", InvTukuiActionBarBackground, "TOPLEFT", 70, 8)
+	
 	if unit == "focus" then		
 		self:ClearAllPoints()
 		self:SetPoint("BOTTOMLEFT", InvTukuiActionBarBackground, "TOPLEFT", 70, 300)
@@ -145,81 +128,3 @@ TukuiMinimap:Point("TOPRIGHT", UIParent, "TOPRIGHT", -20, -20)
 local PlayerProccs = CreateFrame("Frame", "FilgerPlayerProccs", UIParent)
 PlayerProccs:Size(50, 50)
 PlayerProccs:Point("BOTTOMRIGHT", TukuiPlayer, "TOPRIGHT", 0, 4)
-
----------------------------------------------------------------
--- Classbar
----------------------------------------------------------------
--- Check if classbar exists
-
-local function CheckClassbar()
-	--[[if( T.myclass == "WARLOCK" ) then
-		local cb = G.UnitFrames.Player.WarlockSpecBars:IsVisible()
-	elseif( T.myclass == "DRUID" ) then
-		local cb = G.UnitFrames.Player.EclipseBar:IsVisible()
-	elseif( T.myclass == "DEATHKNIGHT" ) then
-		local cb = G.UnitFrames.Player.Runes:IsVisible()
-	elseif( T.myclass == "SHAMAN" ) then
-		local cb = G.UnitFrames.Player.TotemBar1:IsVisible()
-	elseif( T.myclass == "PRIEST" ) then
-		local cb = G.UnitFrames.Player.ShadowOrbsBar:IsVisible()
-	elseif T.myclass == "MONK" then
-		local cb = G.UnitFrames.Player.HarmonyBar:IsVisible()
-		print("Classbar not as")
-	elseif( T.myclass == "PALADIN" ) then
-		local cb = G.UnitFrames.Player.HolyPower:IsVisible()
-	elseif( T.myclass == "ROGUE" or "DRUID") then
-		local cb = G.UnitFrames.Player.ComboPointsBar:IsVisible()
-	elseif( T.myclass == "MAGE" ) then
-		local cb = G.UnitFrames.Player.ArcaneChargeBar:IsVisible()
-	else
-		local cb = false
-	end]]
-	print("Classsasdbar not as")
-	Classbar:SetBackdropColor(1,0,1)
-	--return cb
-	
-	--[[if cb then
-		print("Classbar active")
-	else
-		print("Classbar not active")
-	end]]
-end
-local Classbar = CreateFrame("Frame", "Classbar", TukuiPlayer)
-Classbar:Size(50, 50)
-Classbar:Point("CENTER", UIParent, "CENTER", 0, 0)
-Classbar:SetTemplate()
-Classbar:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
-Classbar:RegisterEvent("PLAYER_ENTERING_WORLD")
-Classbar:SetScript("OnEvent", CheckClassbar)
-Classbar:SetScript("OnEvent", function()
-	local cb
-	if( T.myclass == "WARLOCK" ) then
-		cb = G.UnitFrames.Player.WarlockSpecBars:IsVisible()
-	elseif( T.myclass == "DRUID" ) then
-		cb = G.UnitFrames.Player.EclipseBar:IsVisible()
-	elseif( T.myclass == "DEATHKNIGHT" ) then
-		cb = G.UnitFrames.Player.Runes:IsVisible()
-	elseif( T.myclass == "SHAMAN" ) then
-		cb = G.UnitFrames.Player.TotemBar1:IsVisible()
-	elseif( T.myclass == "PRIEST" ) then
-		cb = G.UnitFrames.Player.ShadowOrbsBar:IsVisible()
-	elseif T.myclass == "MONK" then
-		cb = G.UnitFrames.Player.HarmonyBar:IsVisible()
-		print("Monk ahoy")
-	elseif( T.myclass == "PALADIN" ) then
-		cb = G.UnitFrames.Player.HolyPower:IsVisible()
-	elseif( T.myclass == "ROGUE" or "DRUID") then
-		cb = G.UnitFrames.Player.ComboPointsBar:IsVisible()
-	elseif( T.myclass == "MAGE" ) then
-		cb = G.UnitFrames.Player.ArcaneChargeBar:IsVisible()
-	else
-		cb = false
-	end
-	
-	if cb then
-		print("Classbar active")
-	else
-		print("Classbar not active")
-	end
-end)
-print("Testing")
--- RuzeUI_ConfigUI/config.lua
+++ RuzeUI_ConfigUI/config.lua
@@ -1,70 +1,62 @@
 ﻿local C = {}
 
 C["general"] = {
-	["backdropcolor"] = { .05,.05,.05 },                   -- default backdrop color of panels
-	["bordercolor"] = { .2,.2,.2 },                     -- default border color of panels
-}
-
-C["unitframes"] = {
-	["playerbuffs"] = true,                             -- enable buffs on player unit frame
+	["backdropcolor"] = { .05,.05,.05 },				-- default backdrop color of panels
+	["bordercolor"] = { .2,.2,.2 },						-- default border color of panels
 }
 
 C["actionbar"] = {
-	["buttonsize"] = 32,                                -- normal buttons size
+	["buttonsize"] = 32,								-- normal buttons size
 }
 
 C["classtimer"] = {
-	["enable"] = true,											-- enable classtimer
-	["targetdebuffs"] = false,									-- target debuffs above target (looks crappy imo)
-	["playercolor"] = {.2, .2, .2, 1 },							-- playerbarcolor
-	["targetbuffcolor"] = { 70/255, 150/255, 70/255, 1 },		-- targetbarcolor (buff)
-	["targetdebuffcolor"] = { 150/255, 30/255, 30/255, 1 },		-- targetbarcolor (debuff)
-	["trinketcolor"] = {75/255, 75/255, 75/255, 1 },			-- trinketbarcolor
-	["spark"] = false,
-	["barheight"] = 18,
-	["icon"] = true,
-	["time"] = true,
-	["name"] = false,
+	["enable"] = true,									-- enable classtimer
+	["targetdebuffs"] = false,							-- position target debuffs above target
+	["playercolor"] = {.2, .2, .2 },					-- playerbarcolor
+	["targetbuffcolor"] = { 70/255, 150/255, 70/255 },	-- targetbarcolor (buff)
+	["targetdebuffcolor"] = { 150/255, 30/255, 30/255 },-- targetbarcolor (debuff)
+	["trinketcolor"] = {75/255, 75/255, 75/255 },		-- trinketbarcolor
+	["spark"] = false,									-- enable bar spark
+	["barheight"] = 18,									-- set bar height
+	["icon"] = true,									-- show icons
+	["time"] = true,									-- show time
+	["name"] = false,									-- show spell names
 } 
 
 C["filger"] = {
-	["enable"] = false,
-	["filgerconfig"] = false,
-	["iconsize"] = 32,
-	["proccs"] = "RIGHT",
-	["buffs"] = "RIGHT",
-	["debuffs"] = "RIGHT",
-	["cd"] = "RIGHT",
-	["pet"] = "RIGHT",
+	["enable"] = false,									-- enable filger
+	["filgerconfig"] = false,							-- filger config
+	["iconsize"] = 32,									-- set icon size
+	["proccs"] = "RIGHT",								-- set growth direction
+	["buffs"] = "RIGHT",								-- set growth direction
+	["debuffs"] = "RIGHT",								-- set growth direction
+	["cd"] = "RIGHT",									-- set growth direction
+	["pet"] = "RIGHT",									-- set growth direction
 }
 
 C["datatext"] = {
-	["fps_ms"] = 7,                                     -- show fps and ms on panels
-	["system"] = 0,                                     -- show total memory and others systems infos on panels
-	["bags"] = 0,                                       -- show space used in bags on panels
-	["gold"] = 0,                                       -- show your current gold on panels
-	["wowtime"] = 8,                                    -- show time on panels
-	["guild"] = 0,                                      -- show number on guildmate connected on panels
-	["dur"] = 0,                                        -- show your equipment durability on panels.
-	["friends"] = 0,                                    -- show number of friends connected.
-	["dps_text"] = 0,                                   -- show a dps meter on panels
-	["hps_text"] = 0,                                   -- show a heal meter on panels
-	["power"] = 0,                                      -- show your attackpower/spellpower/healpower/rangedattackpower whatever stat is higher gets displayed
-	["haste"] = 0,                                      -- show your haste rating on panels.
-	["crit"] = 0,                                       -- show your crit rating on panels.
-	["avd"] = 0,                                        -- show your current avoidance against the level of the mob your targeting
-	["armor"] = 0,                                      -- show your armor value against the level mob you are currently targeting
-	["currency"] = 0,                                   -- show your tracked currency on panels
-	["hit"] = 0,                                        -- show hit rating
-	["mastery"] = 0,                                    -- show mastery rating
-	["micromenu"] = 0,                                  -- add a micro menu thought datatext
-	["regen"] = 0,                                      -- show mana regeneration
-	["talent"] = 0,                                     -- show talent
-	["calltoarms"] = 0,                                 -- show dungeon and call to arms
-}
-
-C["skin"] = {
-	["recount"] = true,
+	["fps_ms"] = 7,										-- show fps and ms on panels
+	["system"] = 0,										-- show total memory and others systems infos on panels
+	["bags"] = 0,										-- show space used in bags on panels
+	["gold"] = 0,										-- show your current gold on panels
+	["wowtime"] = 8,									-- show time on panels
+	["guild"] = 0,										-- show number on guildmate connected on panels
+	["dur"] = 0,										-- show your equipment durability on panels.
+	["friends"] = 0,									-- show number of friends connected.
+	["dps_text"] = 0,									-- show a dps meter on panels
+	["hps_text"] = 0,									-- show a heal meter on panels
+	["power"] = 0,										-- show your attackpower/spellpower/healpower/rangedattackpower whatever stat is higher gets displayed
+	["haste"] = 0,										-- show your haste rating on panels.
+	["crit"] = 0,										-- show your crit rating on panels.
+	["avd"] = 0,										-- show your current avoidance against the level of the mob your targeting
+	["armor"] = 0,										-- show your armor value against the level mob you are currently targeting
+	["currency"] = 0,									-- show your tracked currency on panels
+	["hit"] = 0,										-- show hit rating
+	["mastery"] = 0,									-- show mastery rating
+	["micromenu"] = 0,									-- add a micro menu thought datatext
+	["regen"] = 0,										-- show mana regeneration
+	["talent"] = 0,										-- show talent
+	["calltoarms"] = 0,									-- show dungeon and call to arms
 }
 
 TukuiEditedDefaultConfig = C
